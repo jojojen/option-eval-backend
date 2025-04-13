bb3c171d32dbbcfccdd6a6db63655fd2300a275d
jen <jenhung.jocker@gmail.com>
2025-04-13 23:50:38 +0900

    update

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ import numpy as np
 from datetime import datetime, timedelta
 import math
 import logging
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
--- readMe.md
+++ readMe.md
@@ -1,6 +1,7 @@
 python3 -m venv --without-pip
 source venv/bin/activate
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+conda deactivate
 conda activate
 python get-pip.py
 pip --version
